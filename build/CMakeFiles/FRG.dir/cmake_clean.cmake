8157129f33b4207eb654004023e5a3fd5495fce2
kilianroeth <roeth.kilian10@gmail.com>
2026-05-21 23:17:13 +0200

    initial commit

---
+++ build/CMakeFiles/FRG.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FRG.dir/src/main.cpp.o"
+  "CMakeFiles/FRG.dir/src/main.cpp.o.d"
+  "FRG"
+  "FRG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FRG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

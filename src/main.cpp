c2763fd90ce1f0c1297e07cd7dcad322b9d68fe2
kilianroeth <roeth.kilian10@gmail.com>
2026-05-22 20:32:35 +0200

    added classical potential

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 
 int main() {
     
+
+    std::vector<double> V = V_classical();
+
+    std::ofstream out("vector.txt");
+    for (size_t i = 0; i < V.size(); ++i) {
+        out << i/50. << ", " << V[i] << "\n";
+    }
+
     std::cout << "Hello FRG" << std::endl;
 
     return 0;
--- src/phi4_lpa.cpp
+++ src/phi4_lpa.cpp
@@ -13,8 +13,8 @@
 
 // --- Paramters ---------------------------
 
-static double M2        = -0.5;     // classical mass paramters
-static double LAMBDA    = 1.0;      // classical coupling paramter  
+static double M2        = -1.0;     // classical mass paramters
+static double LAMBDA    = 0.5;      // classical coupling paramter  
 
 // RG times t = ln(k/Λ)
 static double T_START   = 0.0;
@@ -24,17 +24,19 @@ static double T_END     = -10.0;
 static int N_RHO        = 500;      // number of rho grid points
 static int RHO_MAX      = 10.0;     // maximal value for rho
 
+
+
 // --- Finite difference derivatives -------
 
-double d_rho;
+static double d_rho;
 
-double dV(std::vector<double> V, int i) {
+inline double dV(std::vector<double> V, int i) {
     if (i == 0) { return (V[1] - V[0])/d_rho; }
-    if (i = N_RHO) { return (V[N_RHO] - V[N_RHO-1])/d_rho; }
+    if (i == N_RHO) { return (V[N_RHO] - V[N_RHO-1])/d_rho; }
     return (V[i+1] - V[i-1])/(2*d_rho);
 }
 
-double ddV(std::vector<double> V, int i) {
+inline double ddV(std::vector<double> V, int i) {
     if (i == 0) { return (V[2] - 2.0*V[1] + V[0])/(d_rho*d_rho); }
     if (i == N_RHO) { return (V[N_RHO] - 2.0*V[N_RHO-1] + V[N_RHO-2])/(d_rho*d_rho); }
     return (V[i+1] - 2.0*V[i] + V[i-1])/(d_rho*d_rho);
@@ -42,6 +44,17 @@ double ddV(std::vector<double> V, int i) {
 
 
 // --- Compute RHS -------------------------
+
+inline std::vector<double> V_classical() {
+    std::vector<double> V;
+    V.reserve(N_RHO);
+
+    for (size_t i = 0; i < N_RHO; ++i) {
+        V.push_back(M2*i/RHO_MAX + LAMBDA/6.0*i/RHO_MAX*i/RHO_MAX);
+    }
+
+    return V;
+}
 
 
 // --- Integrate RG time step --------------

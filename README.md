a627215bea6d7065274d3fe974030aafab078940
kilianroeth <roeth.kilian10@gmail.com>
2026-08-29 00:14:18 +0200

    changed readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # FRG - Functional Renormalization Group
 
 Trying to implement some FRG numerics from scratch.
+
+Worked on some $\phi^4 O(N)$ theory in vacuum.
+
+![effective potential flow](/results/eff_pot_flow_3.pdf)
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include "phi4_lpa.hpp"
 #include "utils.hpp"
 
-int main() {
+int main()
+{
 
     Grid grid;
-    grid.set_rho_vals(linspace(0.,0.75,1000));
+    grid.set_rho_vals(linspace(0., 0.75, 1000));
 
     Params p;
     p.grid = grid;
@@ -23,19 +24,19 @@ int main() {
     cfg.rel_tol = 1e-10;
     cfg.show_progress = true;
 
-    
     std::vector<double> V_init = V_classical(p);
     std::cout << "Classical minimum: V_min = " << V_min_classical(p) << std::endl;
     std::vector<double> dV_vals(V_init.size()), ddV_vals(V_init.size());
-    for (size_t i = 0; i < V_init.size(); ++i) {
-        dV_vals[i] = grid.d1(V_init,i);
-        ddV_vals[i] = grid.d2(V_init,i);
+    for (size_t i = 0; i < V_init.size(); ++i)
+    {
+        dV_vals[i] = grid.d1(V_init, i);
+        ddV_vals[i] = grid.d2(V_init, i);
     }
-    std::vector<double> RHS_vals = RHS(V_init,1,p);
-    save_V(V_init, "results/V_classical.txt",p);
-    save_V(dV_vals, "results/V_classical_prime.txt",p);
-    save_V(ddV_vals, "results/V_classical_doubleprime.txt",p);
-    save_V(RHS_vals, "results/RHS.txt",p);
+    std::vector<double> RHS_vals = RHS(V_init, 1, p);
+    save_V(V_init, "results/V_classical.txt", p);
+    save_V(dV_vals, "results/V_classical_prime.txt", p);
+    save_V(ddV_vals, "results/V_classical_doubleprime.txt", p);
+    save_V(RHS_vals, "results/RHS.txt", p);
 
     integrate_flow_adaptive(V_init, -0.0001, p, cfg, "results/flow_adaptive.csv", 100);
 
